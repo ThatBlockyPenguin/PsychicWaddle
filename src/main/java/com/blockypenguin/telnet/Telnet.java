9abff70764fa5240c06928da48e8822fc1f3fc6a
ThatBlockyPenguin <me@blockypenguin.com>
2023-03-29 15:44:13 +0100

    Switch to ServerSocket-based implementation

--- src/main/java/com/blockypenguin/telnet/Telnet.java
+++ src/main/java/com/blockypenguin/telnet/Telnet.java
@@ -1,25 +1,47 @@
 package com.blockypenguin.telnet;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Telnet {
+	private static final String CHARSET = "UTF-8";
+	
 	public static void main(String[] args) {
-		String url = "localhost";
 		int port = 23;
 
-		try(Socket pingSocket = new Socket(url, port)) { // Open new socket
-			try(
-				PrintWriter out = new PrintWriter(pingSocket.getOutputStream(), true); // Create PrintWriter (output) for socket
-				BufferedReader in = new BufferedReader(new InputStreamReader(pingSocket.getInputStream())) // Create BufferedReader (input) for socket
-			) {
-				out.println("ping"); // Send "ping" over socket
-				System.out.println("Telnet Success: " + in.readLine());
+		try(ServerSocket serverSocket = new ServerSocket(port)) {
+			Socket connectionSocket = serverSocket.accept();
+
+			//Create IO for the connection
+			InputStream input = connectionSocket.getInputStream();
+			OutputStream output = connectionSocket.getOutputStream();
+
+			try(Scanner scanner = new Scanner(input, CHARSET)) {
+				System.out.println("Telnet Success!");
+				
+				PrintWriter out = new PrintWriter(new OutputStreamWriter(output, CHARSET), true);
+				
+				// Output to the server
+				out.println("Hello World! Enter Peace to exit.");
+
+				// Echo back input from the client
+				boolean done = false;
+
+				while(!done && scanner.hasNextLine()) {
+					String line = scanner.nextLine();
+					out.println("Echo from Server: " + line);
+
+					if(line.toLowerCase().trim().equalsIgnoreCase("peace"))
+						done = true;
+				}
 			}
-		}catch(IOException e) { // If socket IO creation failed
+		}catch(IOException e) {
 			System.out.println("Telnet Fail: " + e.getMessage());
 		}
 	}
